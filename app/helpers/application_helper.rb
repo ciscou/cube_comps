d1079d8ba982c166e8b7091a3d79a305e5124ae5
ciscou <francismpp@gmail.com>
2012-03-01 20:34:34 +0100

    added flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def flash_messages
+    flash.map do |k, v|
+      content_tag :div, v, :class => div_class_for_flash_type(k)
+    end.join.html_safe
+  end
+
+  private
+
+  def div_class_for_flash_type(flash_type)
+    case flash_type
+    when :notice then "alert alert-success"
+    when :alert  then "alert alert-error"
+    else flash_type
+    end
+  end
 end
