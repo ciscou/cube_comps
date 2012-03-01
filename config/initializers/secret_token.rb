ced00d93578cdb10b9b3dd8c409b41fa63edb152
ciscou <francismpp@gmail.com>
2012-03-01 19:24:59 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CubeComps::Application.config.secret_token = '1376530a93d850dcb1d87fd09235d484b15c8dbf1d0188bede96d6f7dc3878b5fdc5cb3a6e926e856d4c07b4308ba47080a681571c4f3e0587545c368b542a37'
