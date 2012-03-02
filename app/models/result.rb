8d83b11304bddce3e74dc85b066e9dbf615c9233
Francis <f.perez@flowlab.biz>
2012-03-02 12:24:53 +0100

    autocalculate result's best worst and average

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,4 +1,17 @@
 class Result < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
+
+  before_validation :calculate_best_worst_and_average
+
+  private
+
+  def calculate_best_worst_and_average
+    times = (1..5).map { |i| send("time#{i}") }
+    if times.all?
+      self.best    = times.min
+      self.worst   = times.max
+      self.average = (times.sum - best - worst) / (times.count - 2)
+    end
+  end
 end
