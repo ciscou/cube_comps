d58e8e1ed45e351c77bb71031da9f61876a2ada4
ciscou <francismpp@gmail.com>
2012-03-02 08:00:32 +0100

    user cannot entry events that are not taking place in the competition

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,4 +2,14 @@ class Competition < ActiveRecord::Base
   belongs_to :user
 
   default_scope order(:start_on).reverse_order
+
+  def events
+    Event.all.select { |e| has_event? e }
+  end
+
+  private
+
+  def has_event?(event)
+    send("has_event_#{event.respond_to?(:code) ? event.code : event}?")
+  end
 end
