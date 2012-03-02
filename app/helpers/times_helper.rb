f54c4009a5b29bc49af3221203ee571861d457d4
ciscou <francismpp@gmail.com>
2012-03-02 20:05:24 +0100

    Oops, was failing when formatting nil times

--- app/helpers/times_helper.rb
+++ app/helpers/times_helper.rb
@@ -1,5 +1,7 @@
 module TimesHelper
   def format_time(time)
+    return "-" if time.nil?
+
     minutes, seconds = time.divmod(60)
     minutes = minutes > 0.0 ? minutes.to_i : nil
     seconds = number_with_precision(seconds, :precision => 2).rjust(5, "0")
