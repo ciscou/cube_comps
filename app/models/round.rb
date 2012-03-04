403e8d3fca2ca634eef2b4dfac6be9d0c708a85c
ciscou <francismpp@gmail.com>
2012-03-04 15:09:59 +0100

    lazy load round groups

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,20 +1,22 @@
 class Round
   def initialize(event, number)
     @event, @number = event, number
-    @groups_count = @event.results.by_round(@number).maximum(:group) || 1
   end
 
   def save!
-    @event.create_results_for_round! @number, @groups_count, @entries_count
+    @event.create_results_for_round! @number, groups_count, @entries_count
   end
 
   def groups
-    (1..@groups_count).map do |group|
+    (1..groups_count).map do |group|
       Group.new(self, group)
     end
   end
 
-  attr_accessor :groups_count
+  def groups_count
+    @groups_count ||= @event.results.by_round(@number).maximum(:group) || 1
+  end
+
   attr_accessor :entries_count
 
   attr_reader :number
